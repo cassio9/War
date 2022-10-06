6ac33dc1962b867712e4b406ac9ade29e3d4334d
Cassio Soares <cassiosoares9@hotmail.com>
2022-10-07 01:23:17 +0900

    improve

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
+import { getWinner } from "./utils.js";
 import "./App.css";
 
 function App() {
-  // Variable to control cards from API
   const [cards, setCards] = useState({
-    remainingCards: "",
+    round: 0,
+    remainingRounds: 5,
     ComputerCardImg: "",
     UserCardImg: "",
     ComputerCard: "",
@@ -13,70 +14,27 @@ function App() {
     isPlaying: false,
     isFinished: false,
   });
-
-  //Variables to control score
-  const [scoreComputer, setScoreComputer] = useState(0);
-  const [scoreUser, setScoreUser] = useState(0);
-
-  function newDeck() {
-    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-      .then((res) => res.json())
-      .then((data) => setCards({ ...cards, deckId: data.deck_id }));
-  }
-
-  function drawCards() {
-    if (cards.deckId) {
-      fetch(`https://deckofcardsapi.com/api/deck/${cards.deckId}/draw/?count=2`)
-        .then((res) => res.json())
-        .then((data) => {
-          setCards({
-            ...cards,
-            ComputerCardImg: data.cards[0].image,
-            UserCardImg: data.cards[1].image,
-            remainingCards: data.remaining - 42,
-            ComputerCard: data.cards[0].value,
-            UserCard: data.cards[1].value,
-            isPlaying: true,
-          });
-        });
-    } else {
-      document.getElementById("draw-btn").innerText = "First, shuffle the deck";
-      setTimeout(() => {
-        document.getElementById("draw-btn").innerText = "DRAW";
-      }, 1000);
-    }
-  }
+  const [score, setScore] = useState({ computer: 0, user: 0 });
 
   useEffect(() => {
-    getWinner(cards.ComputerCard, cards.UserCard);
-    finishGame(cards.remainingCards, cards.isPlaying);
-  }, [cards.remainingCards]);
-
-  function getWinner(cardPC, cardMe) {
-    const valueOptions = [
-      "ACE",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "JACK",
-      "QUEEN",
-      "KING",
-    ];
-    const cardPCIndex = valueOptions.indexOf(cardPC);
-    const cardMeIndex = valueOptions.indexOf(cardMe);
-    console.log(cardPCIndex, cardMeIndex);
-    if (cardPCIndex > cardMeIndex) {
-      setScoreComputer(scoreComputer + 1);
-    } else if (cardPCIndex < cardMeIndex) {
-      setScoreUser(scoreUser + 1);
+    async function getCards() {
+      const responseDeckId = await fetch(
+        "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+      );
+      const deckId = await responseDeckId.json();
+      const responseCards = await fetch(
+        `https://deckofcardsapi.com/api/deck/${deckId.deck_id}/draw/?count=52`
+      );
+      const data = await responseCards.json();
+      setCards((prev) => ({ ...prev, data }));
     }
-  }
+    getCards();
+  }, []);
+
+  useEffect(() => {
+    getWinner(cards.ComputerCard, cards.UserCard, setScore);
+    finishGame(cards.remainingCards, cards.isPlaying);
+  }, [cards.round]);
 
   function finishGame(remainingCards, isPlaying) {
     if (remainingCards == 0 && isPlaying) {
@@ -99,56 +57,65 @@ function App() {
       isPlaying: false,
       isFinished: false,
     });
-    setScoreComputer(0);
-    setScoreUser(0);
+    setScore({ computer: 0, user: 0 });
+  }
+
+  console.log(cards);
+  function draw() {
+    if (cards.round < cards.remainingRounds) {
+      setCards((prevState) => ({
+        ...prevState,
+        round: prevState.round + 1,
+        ComputerCardImg: cards.data.cards[`${cards.round}`].image,
+        UserCardImg: cards.data.cards[`${51 - cards.round}`].image,
+        ComputerCard: cards.data.cards[`${cards.round}`].value,
+        UserCard: cards.data.cards[`${51 - cards.round}`].value,
+      }));
+      updateScore();
+    }
+  }
+
+  function updateScore() {
+    const roundWinner = getWinner(cards.ComputerCard, cards.UserCard);
+    if (roundWinner == "PC") {
+      setScore((prevState) => ({
+        ...prevState,
+        computer: prevState.computer + 1,
+      }));
+    } else if (cardPCIndex < cardMeIndex) {
+      setScore((prevState) => ({ ...prevState, computer: prevState.user + 1 }));
+    } else {
+      console.log("draw");
+    }
   }
 
   return (
     <div className="App">
-      <div>
-        <button
-          onClick={cards.isFinished ? resetGame : newDeck}
-          className="btn shuffle"
-        >
-          Shuffle Deck
+      <div className="game-container">
+        <div>
+          <button className="btn shuffle">Start Game</button>
+          <span className="remaining">
+            Round: {cards.round}/{cards.remainingRounds || 0}
+          </span>
+        </div>
+        <div className="main-container">
+          <div className="slot-container">
+            <p>PC: {score.computer}</p>
+            <div className="card-slot">
+              <img src={cards.ComputerCardImg} alt="" />
+            </div>
+          </div>
+          <div className="slot-container">
+            <p className="me-paragraph">User: {score.user}</p>
+            <div className="card-slot">
+              <img src={cards.UserCardImg} alt="" />
+            </div>
+          </div>
+        </div>
+        <button className="btn draw" id="draw-btn" onClick={draw}>
+          {cards.isFinished ? "RESTART" : "Draw"}
         </button>
-        <span className="remaining">
-          Remaining cards: {cards.remainingCards || 0}{" "}
-        </span>
       </div>
-      <h1 id="title" className="title">
-        {scoreComputer > scoreUser && cards.isFinished
-          ? "COMPUTER WON THIS GAME!!!!"
-          : scoreComputer < scoreUser && cards.isFinished
-          ? "USER WON THIS GAME!!!!"
-          : scoreComputer == scoreUser && cards.isFinished
-          ? "TIE!!!!"
-          : "WAR!"}
-      </h1>
-      <div className="main-container">
-        <div className="slot-container">
-          <p>Computer: {scoreComputer}</p>
-          <div className="card-slot">
-            <img src={cards.ComputerCardImg} alt="" />
-          </div>
-        </div>
-        <div className="slot-container">
-          <p className="me-paragraph">User: {scoreUser}</p>
-          <div className="card-slot">
-            <img src={cards.UserCardImg} alt="" />
-          </div>
-        </div>
-      </div>
-      {!cards.deckId && (
-        <p className="startWarning">First, Shuffle the deck </p>
-      )}
-      <button
-        onClick={cards.isFinished ? resetGame : drawCards}
-        className="btn draw"
-        id="draw-btn"
-      >
-        {cards.isFinished ? "RESTART" : "Draw"}
-      </button>
     </div>
   );
 }
